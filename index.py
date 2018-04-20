dcf48d73b453d527244752d1ddbdd7f6aaf703ae
kuzma <v.kuzma@mail.ru>
2018-04-20 18:00:05 +0300

    Add chmod

--- index.py
+++ index.py
@@ -57,13 +57,19 @@ repair_list = ex1c.repairs()
 dt_file = time.strftime("%y%m%d%H%M", time.localtime())
 
 if user_list:
-    csv_writer(user_list, path_file+"/site/user/site_user"+dt_file+".csv")
+    f_user = path_file+"/site/user/site_user"+dt_file+".csv"
+    csv_writer(user_list, f_user)
+    os.chmod(f_user, 0o775)
 
 if order_list:
-    csv_writer(order_list, path_file+"/site/order/site_order"+dt_file+".csv")
+    f_order = path_file+"/site/order/site_order"+dt_file+".csv"
+    csv_writer(order_list, f_order)
+    os.chmod(f_order, 0o775)
 
 if repair_list:
-    csv_writer(repair_list, path_file+"/site/repair/site_repair"+dt_file+".csv")
+    f_repair = path_file+"/site/repair/site_repair"+dt_file+".csv"
+    csv_writer(repair_list, f_repair)
+    os.chmod(f_repair, 0o775)
 
 file_user = os.listdir(path_file+"/1c/user/")
 file_order = os.listdir(path_file + "/1c/order/")
@@ -72,29 +78,32 @@ file_repair = os.listdir(path_file + "/1c/repair/")
 if len(file_user) > 0:
     users = sorted(file_user)
     for user in users:
-        try:
-            updateUser(path_file + "/1c/user/" + user)
-            os.remove(path_file + "/1c/user/" + user)
-        except:
-            continue
+        updateUser(path_file + "/1c/user/" + user)
+        #try:
+         #   updateUser(path_file + "/1c/user/" + user)
+          #  os.remove(path_file + "/1c/user/" + user)
+        #except:
+         #   continue
 
 if len(file_order) > 0:
     orders = sorted(file_order)
     for order in orders:
-        try:
-            updateOrder(path_file + "/1c/order/" + order)
-            os.remove(path_file + "/1c/order/" + order)
-        except:
-            continue
+        updateOrder(path_file + "/1c/order/" + order)
+        #try:
+         #   updateOrder(path_file + "/1c/order/" + order)
+          #  os.remove(path_file + "/1c/order/" + order)
+        #except:
+         #   continue
 
 if len(file_repair) > 0:
     repairs = sorted(file_repair)
     for repair in repairs:
-        try:
-            updateRepair(path_file + "/1c/repair/" + repair)
-            os.remove(path_file + "/1c/repair/" + repair)
-        except:
-            continue
+        updateRepair(path_file + "/1c/repair/" + repair)
+        #try:
+         #   updateRepair(path_file + "/1c/repair/" + repair)
+          #  os.remove(path_file + "/1c/repair/" + repair)
+        #except:
+         #   continue
 
 dt = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 ex1c.updateTimeExchange(dt)
